a7b9352747282675d24247de16ee4c65d6c8b806
Saurabh750 <60783291+Saurabh750@users.noreply.github.com>
2020-06-15 17:41:20 +0530

    Add files via upload

---
+++ Fibonaccinum.cpp
@@ -0,0 +1,32 @@
+
+
+#include <iostream>
+#include <vector>
+#include "Fibonaccinum.h"
+using namespace std;
+
+long long Fibonum() {
+    int n;
+    cin >> n;
+    if(n < 2)
+    {
+        return(n);
+    }
+    else
+    {
+        vector<long long> myfib[n + 1] = {};
+        myfib[0] = 0;
+        myfib[1] = 1;
+        for (int i = 2; i < n; i++)
+        {
+            myfib[i] = myfib[i - 1] + myfib[i - 2];
+        }
+        return(myfib[n]);
+    }
+}
+
+int main()
+{
+    cout << Fibonum() << endl;
+    return 0;
+}
